36e7d1d3ea09eae3b913ac8351df01c7b0c3ce10
Sergei Snitko <sergeisnit@yandex.ru>
2023-01-12 02:05:32 +0300

    Версия 2. Код отформатирован отступами

--- music_site.sql
+++ music_site.sql
@@ -1,48 +1,53 @@
 
 CREATE TABLE IF NOT EXISTS Styles (
-	id_style SERIAL PRIMARY KEY,
-	style_name VARCHAR(40) UNIQUE NOT NULL
+	PRIMARY KEY (id_style),
+	id_style 		SERIAL,
+	style_name 		VARCHAR (40) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Artists (
-	id_artist SERIAL PRIMARY KEY,
-	artist_name VARCHAR(80) UNIQUE NOT NULL
+	PRIMARY KEY (id_artist),
+	id_artist 		SERIAL,
+	artist_name 	VARCHAR (80) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Styles_Artists (
-	id_style INTEGER NOT NULL REFERENCES Styles(id_style),
-	id_artist INTEGER NOT NULL REFERENCES Artists(id_artist),
+	id_style 		INTEGER NOT NULL REFERENCES Styles(id_style),
+	id_artist 		INTEGER NOT NULL REFERENCES Artists(id_artist),
 	PRIMARY KEY(id_style, id_artist)
 );
 
 CREATE TABLE IF NOT EXISTS Albums (
-	id_album SERIAL PRIMARY KEY,
-	album_name VARCHAR(80) UNIQUE NOT NULL,
-	year_produced integer NOT NULL CHECK(year_produced > 1900)
+	PRIMARY KEY (id_album),
+	id_album 		SERIAL,
+	album_name 		VARCHAR (80) UNIQUE NOT NULL,
+	year_produced 	INTEGER NOT NULL CHECK(year_produced > 1900)
 );
 
 CREATE TABLE IF NOT EXISTS Artists_Albums (
-	id_artist INTEGER NOT NULL REFERENCES Artists(id_artist),
-	id_album INTEGER NOT NULL REFERENCES Albums(id_album),
+	id_artist 		INTEGER NOT NULL REFERENCES Artists(id_artist),
+	id_album 		INTEGER NOT NULL REFERENCES Albums(id_album),
 	PRIMARY KEY(id_artist, id_album)
 );
 
 CREATE TABLE IF NOT EXISTS Tracks (
-	id_track SERIAL PRIMARY KEY,
-	track_name VARCHAR(100) NOT NULL,
-	duration INTEGER NOT NULL CHECK(duration > 0),
-	id_album INTEGER NOT NULL REFERENCES Albums(id_album)
+	PRIMARY KEY (id_track),
+	id_track 		SERIAL,
+	track_name 		VARCHAR (100) NOT NULL,
+	duration 		INTEGER NOT NULL CHECK(duration > 0),
+	id_album 		INTEGER NOT NULL REFERENCES Albums(id_album)
 );
 
 CREATE TABLE IF NOT EXISTS Collections (
-	id_collection SERIAL PRIMARY KEY,
-	collection_name VARCHAR(80) UNIQUE NOT NULL,
-	year_produced integer NOT NULL CHECK(year_produced > 1900)
+	PRIMARY KEY (id_collection),
+	id_collection 	SERIAL,
+	collection_name VARCHAR (80) UNIQUE NOT NULL,
+	year_produced 	INTEGER NOT NULL CHECK(year_produced > 1900)
 );
 
 CREATE TABLE IF NOT EXISTS Tracks_Collections (
-	id_collection INTEGER NOT NULL REFERENCES Collections(id_collection),
-	id_track INTEGER NOT NULL REFERENCES Tracks(id_track),
+	id_collection 	INTEGER NOT NULL REFERENCES Collections(id_collection),
+	id_track 		INTEGER NOT NULL REFERENCES Tracks(id_track),
 	PRIMARY KEY(id_collection, id_track)
 );
 
--- select_requests.sql
+++ select_requests.sql
@@ -1,19 +1,19 @@
 SELECT album_name, year_produced FROM albums
-WHERE year_produced = 2018;
+ WHERE year_produced = 2018;
 
 SELECT track_name, duration FROM tracks
-ORDER BY duration DESC
-LIMIT 1;
+ ORDER BY duration DESC
+ LIMIT 1;
 
 SELECT track_name FROM tracks
-WHERE duration >= 3.5 * 60;
+ WHERE duration >= 3.5 * 60;
 
 SELECT collection_name FROM collections
-WHERE year_produced BETWEEN 2018 AND 2020;
+ WHERE year_produced BETWEEN 2018 AND 2020;
 
 SELECT artist_name FROM artists
-WHERE artist_name NOT LIKE ('% %');
+ WHERE artist_name NOT LIKE ('% %');
 
 SELECT track_name FROM tracks
-WHERE track_name LIKE ('%my%') OR track_name LIKE ('%My%') OR 
-track_name LIKE ('%мой%') OR track_name LIKE ('%Мой%');
+ WHERE track_name LIKE ('%my%') OR track_name LIKE ('%My%') 
+    OR track_name LIKE ('%мой%') OR track_name LIKE ('%Мой%');
